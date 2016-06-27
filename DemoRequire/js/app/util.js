12091201a7cf272b40a6a2d991d57541e8a25183
xuds <xdsking@163.com>
2016-06-27 11:46:46 +0800

    .

--- DemoRequire/js/app/util.js
+++ DemoRequire/js/app/util.js
@@ -1,16 +1,28 @@
 /**
  * Created by xuds on 2016/1/12.
- * 浏览器特性检查
  */
-define(function(){
+define(["jquery", "require"], function ($, require) {
     return {
         //异步加载css文件
-        loadStyleSheet:function(url){
-            $("head").append($("<link>",{
-                rel:"stylesheet",
-                type:"text/css",
-                href:url
+        loadStyleSheet: function (url) {
+            //获得相对于baseUrl的地址
+            var cssUrl = require.toUrl(url);
+            $("head").append($("<link>", {
+                rel: "stylesheet",
+                type: "text/css",
+                href: cssUrl
             }));
+        },
+        loadTemplate: function (url, position) {
+            var deferred = $.Deferred();
+            var templateUrl = require.toUrl(url);
+            var res = $.get(templateUrl);
+            res.done(function (template) {
+                var $template = $(template);
+                $(position).append($template);
+                deferred.resolve($template);
+            });
+            return deferred;
         }
     }
 });
