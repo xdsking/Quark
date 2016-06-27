6d715c5eea8f64a3e87075b704f1752eeb420bac
xuds <xdsking@163.com>
2016-06-27 16:25:45 +0800

    .

--- DemoRequire/js/main.js
+++ DemoRequire/js/main.js
@@ -2,19 +2,17 @@
  * Created by xuds on 2016/1/12.
  */
 
-requirejs.config({
+require.config({
     baseUrl: 'js/app',
     paths: {
         jquery: "./jQuery-1.11.1"
     }
 });
-requirejs(["jquery"], function ($) {
-    requirejs(["infoWindow/infoWindow"], function (infoWindow) {
-        $("#showInfoWindow").click(function () {
-            infoWindow.show();
-        });
-        $("#hideInfoWindow").click(function () {
-            infoWindow.hide();
-        });
+require(["jquery", "infoWindow/infoWindow"], function ($, infoWindow) {
+    $("#showInfoWindow").click(function () {
+        infoWindow.show();
+    });
+    $("#hideInfoWindow").click(function () {
+        infoWindow.hide();
     });
 });
