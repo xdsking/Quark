21c7daa5dbac79d6eb885b95a30a3e36dc344ae5
xuds <xdsking@163.com>
2016-06-27 11:46:51 +0800

    .

--- DemoRequire/js/main.js
+++ DemoRequire/js/main.js
@@ -5,34 +5,16 @@
 requirejs.config({
     baseUrl: 'js/app',
     paths: {
-        lib: "../../../../CDN/lib",
-        jquery: "../../../../CDN/lib/jQuery/jQuery-1.11.1"
+        jquery: "./jQuery-1.11.1"
     }
 });
-requirejs(["jquery", "tools"], function ($, tools) {
-    requirejs(["util"], function (util) {
-        util.loadStyleSheet("./css/index.css");
+requirejs(["jquery"], function ($) {
+    requirejs(["infoWindow/infoWindow"], function (infoWindow) {
+        $("#showInfoWindow").click(function () {
+            infoWindow.show();
+        });
+        $("#hideInfoWindow").click(function () {
+            infoWindow.hide();
+        });
     });
-    tools.push(1);
-    console.log(tools.getArray());
-    tools.push(2);
-    console.log(tools.getArray());
-    tools.push(3);
-    console.log(tools.getArray());
-    tools.pop();
-    console.log(tools.getArray());
-    tools.pop();
-    console.log(tools.getArray());
-});
-requirejs(["jquery", "util", "tools"], function ($, util, tools) {
-    tools.push(1);
-    console.log(tools.getArray());
-    tools.push(2);
-    console.log(tools.getArray());
-    tools.push(3);
-    console.log(tools.getArray());
-    tools.pop();
-    console.log(tools.getArray());
-    tools.pop();
-    console.log(tools.getArray());
 });
